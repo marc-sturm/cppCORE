0fb6a4b824ab6d92d93093fc81cdbc68a9997c88
Alexandr Chernov <kstchernov@gmail.com>
2026-04-28 15:43:00 +0200

    Changed macro

--- cppCORE_global.h
+++ cppCORE_global.h
@@ -9,13 +9,16 @@
 #  define CPPCORESHARED_EXPORT Q_DECL_IMPORT
 #endif
 
-// #define SET_QT_OFFSCREEN() qputenv("QT_QPA_PLATFORM", "offscreen")
+#ifdef USE_CORE_APP
+#define SET_QT_OFFSCREEN() qputenv("QT_QPA_PLATFORM", "offscreen")
+#else
+#define SET_QT_OFFSCREEN()
+#endif
 
 #ifdef USE_CORE_APP
 #define APP_BASE_CLASS QCoreApplication
 #else
 #define APP_BASE_CLASS QApplication
 #endif
-
 
 #endif // CPPCORE_GLOBAL_H
